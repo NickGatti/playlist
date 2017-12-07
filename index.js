00e8ae9b707e3c37a4773d1819ce454984e6bef8
NickGatti <nick.gatti@gmail.com>
2017-12-07 14:01:03 -0700

    add tracks to bin

--- index.js
+++ index.js
@@ -50,9 +50,14 @@ function chooseTracks() {
     $( 'header' ).append( '<div class="copy">Click on an album to add its tracks</div>' )
     $( 'main' ).append( "<div class='mainTrackList'></div><div class='trackListBin'></div><button id='clearTracks' class='buttonStyle' type='button' name='button'>Clear Tracks</button><button id='submitBin' class='buttonStyle' type='button' name='button'>Submit Bin</button>" )
     for ( let i = 0; i < data.results.length; i++ ) {
-        $( '.mainTrackList' ).append( `<img src='images/${data.results[i].cover_art}' class='trackListTile'>` )
+        $( '.mainTrackList' ).append( `<img src='images/${data.results[i].cover_art}' class='trackListTile' id='${data.results[ i ].title}' >` )
     }
     $( 'main' ).css( 'padding', '4em' )
     $( 'button' ).css( 'margin-left', '3em' )
     $( 'main' ).css( 'min-height', '86vh' )
+    $( '.trackListTile' ).click( function () {
+        $( '.trackListTile' ).css( 'border', 'none' )
+        $( this ).css( 'border', '3px solid skyblue' )
+        $( '.trackListBin' ).append( `<p>${this.id}</p>` )
+    } )
 }
