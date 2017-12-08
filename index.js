83429fd6b2e6bfaa2a85bbb03142957baf5bda8f
NickGatti <nick.gatti@gmail.com>
2017-12-08 16:10:33 -0700

    these files added after deadline 3pm @4:10pm

--- index.js
+++ index.js
@@ -1,92 +1,103 @@
-let data;
+$( document ).ready( function () {
 
-window.addEventListener( 'load', function () {
-    let xhr = new XMLHttpRequest()
+    window.addEventListener( 'load', function () {
 
-    xhr.addEventListener( 'load', selectFirstThreeAlbums )
+        let xhr = new XMLHttpRequest()
 
-    xhr.open( 'GET', 'http://localhost:3000/api' )
-    xhr.send()
+        xhr.addEventListener( 'load', selectFirstThreeAlbums )
 
-    $( '#chooseTracks' ).click( chooseTracks )
-} )
+        xhr.open( 'GET', 'http://localhost:3000/api' )
+        xhr.send()
 
-function randomNumber( maxSize ) {
-    return Math.floor( Math.random() * maxSize )
-}
+        $( '#chooseTracks' ).click( chooseTracks )
+    } )
 
-function randomNumbersInArray( maxSize ) {
-    let output = []
-    let generator
-    let pass
+    let data;
 
-    while ( output.length != 3 ) {
-        generator = randomNumber( maxSize )
-        pass = true
-        for ( let i = 0; i < output.length; i++ ) {
-            if ( generator === output[ i ] ) {
-                pass = false
+    function randomNumber( maxSize ) {
+        return Math.floor( Math.random() * maxSize )
+    }
+
+    function randomNumbersInArray( maxSize ) {
+        let output = []
+        let generator
+        let pass
+
+        while ( output.length != 3 ) {
+            generator = randomNumber( maxSize )
+            pass = true
+            for ( let i = 0; i < output.length; i++ ) {
+                if ( generator === output[ i ] ) {
+                    pass = false
+                }
             }
+            if ( pass ) output.push( generator )
         }
-        if ( pass ) output.push( generator )
+        return output
     }
-    return output
-}
 
-function selectFirstThreeAlbums() {
-    data = JSON.parse( this.responseText )
-    let albumArray = randomNumbersInArray( data.results.length )
-    for ( let i = 0; i < albumArray.length; i++ ) {
-        addCoverArt( data.results[ albumArray[ i ] ].cover )
+    function selectFirstThreeAlbums() {
+        if ( this.status < 200 && this.status >= 400 && this.readyState !== 1 ) return
+        data = JSON.parse( this.responseText )
+        let albumArray = randomNumbersInArray( data.results.length )
+        for ( let i = 0; i < albumArray.length; i++ ) {
+            addCoverArt( data.results[ albumArray[ i ] ].cover )
+        }
     }
-}
 
-function addCoverArt( argSrc ) {
-    $( '.selectListCont' ).append( `<img class='coverArt' src='${argSrc}'>` )
-}
-
-function chooseTracks() {
-    $( 'main' ).empty()
-    $( 'main' ).css( 'flex-direction', 'column' )
-    $( 'header' ).append( '<div class="copy">Click on an album to add its tracks</div>' )
-    $( 'main' ).append( "<div class='mainTrackListCont'><div class='mainTrackList'></div></div><div class='binContainer'><div class='trackListBin'></div><div class='songListBin'></div></div><div class='btnDiv'><button id='clearTracks' class='buttonStyle' type='button' name='button'>Clear Tracks</button><button id='submitBin' class='buttonStyle' type='button' name='button'>Submit Bin</button><button id='getSongs' class='buttonStyle' type='button' name='button'>Get Songs</button></div>" )
-    for ( let i = 0; i < data.results.length; i++ ) {
-        $( '.mainTrackList' ).append( `<img src='${data.results[i].cover}' class='trackListTile' id='${[i]}' >` )
+    function addCoverArt( argSrc ) {
+        $( '.selectListCont' ).append( `<img class='coverArt' src='${argSrc}'>` )
     }
-    $( 'main' ).css( 'padding', '4em' )
-    $( 'button' ).css( 'margin-left', '3em' )
-    $( 'main' ).css( 'min-height', '86vh' )
+
+    function chooseTracks() {
+        $( 'main' ).empty()
+        $( 'main' ).css( 'flex-direction', 'column' )
+        $( 'header' ).append( '<div class="copy">Click on an album to add its tracks</div>' )
+        $( 'main' ).append( "<div class='mainTrackListCont'><div class='mainTrackList'></div></div><div class='binContainer'><div class='trackListBin'></div><div class='songListBin'></div></div><div class='btnDiv'><button id='clearTracks' class='buttonStyle' type='button' name='button'>Clear Tracks</button><button id='submitBin' class='buttonStyle' type='button' name='button'>Submit Bin</button><button id='getSongs' class='buttonStyle' type='button' name='button'>Get Songs</button></div>" )
+        for ( let i = 0; i < data.results.length; i++ ) {
+            $( '.mainTrackList' ).append( `<img src='${data.results[i].cover}' class='trackListTile' id='${[i]}' >` )
+        }
+        $( 'main' ).css( 'padding', '4em' )
+        $( 'button' ).css( 'margin-left', '3em' )
+        $( 'main' ).css( 'min-height', '86vh' )
 
 
-    let postObj = {}
-    let tileClicked
-    $( '.trackListTile' ).click( function () {
-        tileClicked = this.id
-        $( '.trackListTile' ).css( 'border', 'none' )
-        $( this ).css( 'border', '3px solid skyblue' )
-        $( '.trackListBin' ).append( `<p>${data.results[this.id].artist}: ${data.results[this.id].album}</p>` )
-        postObj[ data.results[ this.id ].artist ] = data.results[ this.id ].album
-    } )
-    $( '#clearTracks' ).click( function () {
-        $( '.trackListBin' ).empty()
-    } )
-    $( '#submitBin' ).click( () => {
-        $.post( 'https://lit-fortress-6467.herokuapp.com/post', postObj ).done( function ( data ) {
-            $( '.trackListBin' ).empty()
-            $( '.trackListBin' ).append( `<p>${data}</p>` )
+        let postObj = {}
+        let tileClicked
+        $( '.trackListTile' ).click( function () {
+            tileClicked = this.id
+            $( '.trackListTile' ).css( 'border', 'none' )
+            $( this ).css( 'border', '3px solid skyblue' )
+            $( '.trackListBin' ).append( `<p>${data.results[this.id].artist}: ${data.results[this.id].album}</p>` )
+            postObj[ data.results[ this.id ].artist ] = data.results[ this.id ].album
         } )
-    } )
-    $( '#getSongs' ).click( () => {
-        $( '.songListBin' ).empty()
-        for ( let key in postObj ) {
-            $( '.songListBin' ).append( `<h3>Artist: ${key}</h3>` )
-            for ( let i = 0; i < data.results.length; i++ ) {
-                if ( key === data.results[ i ].artist ) {
-                    for ( let z = 0; z < data.results[ i ].tracklist.length; z++ ) {
-                        $( '.songListBin' ).append( `<p>${data.results[i].tracklist[z]}</p>` )
+        $( '#clearTracks' ).click( function () {
+            postObj = {}
+            $( '.trackListBin' ).empty()
+        } )
+        $( '#submitBin' ).click( () => {
+            $.post( 'https://lit-fortress-6467.herokuapp.com/post', postObj ).done( function ( data ) {
+                $( '.trackListBin' ).empty()
+                $( '.songListBin' ).empty()
+                $( '.trackListBin' ).append( `<p>${data}</p>` )
+            } )
+        } )
+        $( '#getSongs' ).click( () => {
+            if ( Object.keys( postObj ).length === 0 && postObj.constructor === Object ) {
+                $( '.songListBin' ).empty()
+                return;
+            }
+            $( '.songListBin' ).empty()
+            for ( let key in postObj ) {
+                $( '.songListBin' ).append( `<h3>Artist: ${key}</h3>` )
+                for ( let i = 0; i < data.results.length; i++ ) {
+                    if ( key === data.results[ i ].artist ) {
+                        for ( let z = 0; z < data.results[ i ].tracklist.length; z++ ) {
+                            $( '.songListBin' ).append( `<p>${data.results[i].tracklist[z]}</p>` )
+                        }
                     }
                 }
             }
-        }
-    } )
-}
+        } )
+    }
+} )
